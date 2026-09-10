f6bb723e0ae3467667d94d2954e94e3f05a358ce
aeoniandreams <165136891+aeoniandreams@users.noreply.github.com>
2026-09-11 00:00:02 +0900

    Update firebase-config.js

--- js/firebase-config.js
+++ js/firebase-config.js
@@ -2,15 +2,15 @@
 // "내 앱" 섹션에서 웹 앱을 추가하면 아래와 같은 형태의 설정 값을 받을 수 있어요.
 // 그 값을 그대로 이 자리에 붙여넣으면 됩니다.
 window.FIREBASE_CONFIG = {
-  apiKey: "YOUR_API_KEY",
-  authDomain: "YOUR_PROJECT_ID.firebaseapp.com",
-  projectId: "YOUR_PROJECT_ID",
-  storageBucket: "YOUR_PROJECT_ID.firebasestorage.app",
-  messagingSenderId: "YOUR_SENDER_ID",
-  appId: "YOUR_APP_ID"
+  apiKey: "AIzaSyDwqT_kLLINGh1yxSTtUQoRyIdlfn8cvp4",
+  authDomain: "AIzaSyDwqT_kLLINGh1yxSTtUQoRyIdlfn8cvp4",
+  projectId: "lookbook-a4512",
+  storageBucket: "lookbook-a4512.firebasestorage.app",
+  messagingSenderId: "697597192526",
+  appId: "1:697597192526:web:90b00afaf6caef3740561f",
 };
 
 // 지인들이 입장 화면에서 입력할 "공용 비밀번호"는 Firebase Authentication에
 // 이메일/비밀번호 사용자 한 명을 만들어서 관리해요. 그 사용자의 이메일 주소를
 // 여기에 적어주세요. (이메일 자체는 화면에 노출되지 않고, 코드 안에서만 쓰여요)
-window.FIREBASE_AUTH_EMAIL = "guest@lookbook.local";
+window.FIREBASE_AUTH_EMAIL = "ae0niandreams@gmail.com";
